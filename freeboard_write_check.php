9947fba3064f92e9322c2e03e4eec10aba4b3536
Einstrasse <hg9587@naver.com>
2017-08-10 13:38:47 +0000

    자유게시판 글 작성

--- freeboard_write_check.php
+++ freeboard_write_check.php
@@ -1,8 +1,32 @@
 <?php
+	include_once('./include/no_not_logined.php');
 	include_once('./include/db_conn.php');
+	header("Content-Type: text/html; charset=UTF-8");
 
 	if (isset($_POST['title']) && isset($_POST['contents'])) {
+		$writer_id=$_SESSION['id'];
 		
+		$title=$_POST['title'];
+		$contents=addslashes($_POST['contents']);
+		
+		$sql="INSERT INTO `freeboard_item` (`title`, `writer_id`, `contents`) VALUES('".$title."', '".$writer_id."', '".$contents."')";
+		
+		mysql_query($sql) or die('글 등록 실패');
+		?>
+		<html>
+			<head>
+				<meta charset="utf-8">
+			</head>	
+			<body>
+				<script>
+					alert('글이 정상적으로 작성되었습니다.');
+					location.href="./freeboard.php";
+				</script>
+			</body>
+		</html>
+		<?php
+		//echo htmlspecialchars($sql);
+		exit();
 	} else {
 		?>
 		<html>
